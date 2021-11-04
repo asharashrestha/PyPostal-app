f78bf085ee1f5ac89b9140e006be3a4610c62c27
Ashara <ashara.shrestha@gmail.com>
2021-11-04 10:27:05 -0500

    modified requirements and Pypostal file

--- Pypostal.py
+++ Pypostal.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from postal.expand import expand_address
 from postal.parser import parse_address
-import csv
 
 st.set_page_config(layout="wide")
-model =st.sidebar.selectbox("Select library: ",["PyPostal","Address-Net"])
+# model =st.sidebar.selectbox("Select library: ",["PyPostal","Address-Net"])
 file_path = "/Users/ashara/Documents/AtosSyntel/AdressParsr/result_txt.csv"
-if model == "PyPostal":
-    addr = st.sidebar.text_area("Enter Address")
+# if model == "PyPostal":
+addr = st.sidebar.text_area("Enter Address")
+parse_addr = parse_address(addr)
+for elem in parse_addr:
+    st.write(str(elem[1]) + ": " + str(elem[0]))
+st.write("\n")
